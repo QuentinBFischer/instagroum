f29aa76df9309939fd0d7ea913f2bc76e53fb016
Quentin Berger-Fischer <b.fischer.quentin@gmail.com>
2018-03-14 12:18:55 +0100

    initial version of instagroum

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function navAction($route)
+    {
+        return $this->render('navbar.html.twig', array(
+            'navigation' => array(
+                'home'      => array('href' => '/', 'caption' => 'Home'),
+                'test'       => array('href' => '/test', 'caption' => 'Test'),
+                'login'    => array('href' => '/login', 'caption' => 'Login'),
+            ),
+            'route' => $route
+        ));
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
